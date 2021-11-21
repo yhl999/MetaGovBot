ddda26376360dc75420f01188da83f82dd5f1e1f
Yuan Han Li <yuanhan.li@protonmail.com>
2021-11-21 16:52:42 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { watchAave } from "./protocols/aave";
 import { watchCompound } from "./protocols/compound"
 import { watchUniswap } from "./protocols/uniswap";
+import { watchDydx } from "./protocols/dydx";
 import {SnapshotMirror } from "./snapshotMirror/snapshotMirror";
 
 require("dotenv").config();
@@ -8,6 +9,7 @@ require("dotenv").config();
 watchCompound();
 watchAave();
 watchUniswap();
+watchDydx();
 
 const mirror = new SnapshotMirror(process.env.WATCHED_SPACES.split(","));
 mirror.watchSnapshot();
